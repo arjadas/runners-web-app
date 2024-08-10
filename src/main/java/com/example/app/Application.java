42c0d13e06d8fc600ba16a0a9783d373689f767a
arjadas <arjadas17@gmail.com>
2024-08-10 22:29:45 +1000

    Demo using a logger.

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -1,5 +1,7 @@
 package com.example.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,23 +9,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
-
-
-		/* method 1
-
 		SpringApplication.run(Application.class, args);
-		WelcomeMessage welcomeMessage = new WelcomeMessage();
-		System.out.println(welcomeMessage.getWelcomeMessage());
-
-		 */
-
-		// method 2 - Spring's dependency injection
-
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		WelcomeMessage welcomeMessage2 = (WelcomeMessage) context.getBean("welcomeMessage");
-		System.out.println(welcomeMessage2);
-
+		log.info("Application started successfully!");
 	}
 
 }
